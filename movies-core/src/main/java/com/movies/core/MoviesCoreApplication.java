8aa13de5beee546a9b17b0b11efd81833ef81c69
Gustavo Fontana <gustavoasf77@gmail.com>
2023-02-24 01:19:01 -0300

    initial config

---
+++ movies-core/src/main/java/com/movies/core/MoviesCoreApplication.java
@@ -0,0 +1,13 @@
+package com.movies.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesCoreApplication.class, args);
+	}
+
+}
---
+++ movies-core/src/test/java/com/movies/core/MoviesCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
